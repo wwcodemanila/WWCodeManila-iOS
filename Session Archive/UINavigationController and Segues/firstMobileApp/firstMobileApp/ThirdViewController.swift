c641b987c313dbd49c0175db84526f926dbdd8be
Ma. Ciela Salazar <aleic_27@yahoo.com.ph>
2017-10-01 21:02:39 +0800

    UINavigation and Segue app example fix

---
+++ Session Archive/UINavigationController and Segues/firstMobileApp/firstMobileApp/ThirdViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ThirdViewController.swift
+//  firstMobileApp
+//
+//  Created by Ma. Ciela Salazar on 8/21/17.
+//  Copyright © 2017 Ciela Salazar. All rights reserved.
+//
+
+import UIKit
+
+class ThirdViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
